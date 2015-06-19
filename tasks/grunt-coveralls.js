842e5e355402a577ae50d48c1ef0683ece246d64
stanleyn <stanleyhlng@yahoo.com.hk>
2015-06-19 00:24:24 -0700

    updated

--- tasks/grunt-coveralls.js
+++ tasks/grunt-coveralls.js
@@ -4,17 +4,13 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-coveralls');
     grunt.config('coveralls', {
         options: {
-            // LCOV coverage file relevant to every target
-            // src: '<%= workspace %>/artifacts/coverage/lcov.info',
-
             // When true, grunt-coveralls will only print a warning rather than
             // an error, to prevent CI builds from failing unnecessarily (e.g. if
             // coveralls.io is down). Optional, defaults to false.
             force: false
         },
-
         coverage: {
-            src: 'artifacts/coverage/lcov.info'
+            src: '<%= workspace %>/artifacts/coverage/lcov.info'
         }
     });
 };
